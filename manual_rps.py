a9e69fd8ce6dee3fc80bd53e4b46acb141c88602
serlaic <s.laicuks@wesec.co.uk>
2023-02-24 19:44:12 +0000

    moves import randon inside the function get_computer_choice

--- manual_rps.py
+++ manual_rps.py
@@ -1,12 +1,10 @@
-import random as rd
-
 def get_computer_choice():
-    computer_choice = rd.choice(['Rock' , 'Paper', 'Scissors'])
+    import random as rd
+    computer_choice = rd.choice(["Rock" , "Paper", "Scissors"])
     return computer_choice
 
 def get_user_choice():
-    user_choice = input('Choose Paper, Scissors or Rock')
+    user_choice = input("Choose Paper, Scissors or Rock:")
     return user_choice
-
 
 

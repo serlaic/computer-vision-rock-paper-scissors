a0406d7db23461fc2421219cd5d67a718346993d
serlaic <s.laicuks@wesec.co.uk>
2023-02-24 20:31:06 +0000

    changes order of arguments in get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -10,20 +10,21 @@ def get_user_choice():
 def get_winner(computer_choice , user_choice):
     if computer_choice == user_choice:
         return "It is a tie!"
-    elif computer_choice == "Paper" and user_choice == "Rock":
-        return "You Lost"
     elif computer_choice == "Rock" and user_choice == "Scissors":
+        return "You Lost"
+    elif computer_choice == "Paper" and user_choice == "Rock":
         return "You Lost"    
     elif computer_choice == "Scissors" and user_choice == "Paper":
         return "You Lost"   
-    elif user_choice == "Paper" and computer_choice == "Rock":
-        return "You won!"  
     elif user_choice == "Rock" and computer_choice == "Scissors":
+        return "You won!"  
+    elif user_choice == "Paper" and computer_choice == "Rock":
         return "You won!"       
     elif user_choice == "Scissors" and computer_choice == "Paper":
         return "You won!"
 
 print(get_winner(get_computer_choice() , get_user_choice()))
+
 
 
 

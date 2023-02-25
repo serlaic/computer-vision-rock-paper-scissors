0ca135e1b17250bb82932c6d55e1ff73b20a5988
serlaic <s.laicuks@wesec.co.uk>
2023-02-25 00:04:48 +0000

    adds timer for 10 seconds before the result is registered

--- camera_rps.py
+++ camera_rps.py
@@ -5,13 +5,18 @@ def get_computer_choice():
     return computer_choice
 
 def get_prediction():
+    import time
     import cv2
     from keras.models import load_model
     import numpy as np
+    start_time = time.time()
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     while True: 
+        timer = time.time() - start_time
+        if timer > 10:
+            break
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
@@ -19,19 +24,20 @@ def get_prediction():
         data[0] = normalized_image
         prediction = model.predict(data)
         cv2.imshow('frame', frame)
-# Press q to close the window
         user_choice = np.argmax(prediction)
-        if user_choice == 0:
-            print("Rock")
-            return "Rock"
-        elif user_choice == 1:
-            print("Paper")
-            return "Paper"
-        elif user_choice == 2:
-            print("Scissors")
-            return "Scissors"
+# Press q to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break            
+            break
+        print(time.time())  
+    if user_choice == 0:
+        print("Rock")
+        return "Rock"
+    elif user_choice == 1:
+        print("Paper")
+        return "Paper"
+    elif user_choice == 2:
+        print("Scissors")
+        return "Scissors"          
 # After the loop release the cap object
     print(user_choice)
     cap.release()
